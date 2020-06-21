fbf0e96d95a45cfea6574dca800d2ec57811ae1e
George Gecewicz <875201+ggwicz@users.noreply.github.com>
2020-06-21 14:26:41 -0400

    initial commit: scaffolding plugin via wp-cli scaffold command

---
+++ wp-dashboard-heatmap.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Wp Dashboard Heatmap
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     wp-dashboard-heatmap
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Wp_Dashboard_Heatmap
+ */
+
+// Your code starts here.
